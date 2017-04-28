42b0b325d80730c03b6e9088a227b663343594d7
Cosmic Web Services <andrewrodebaugh@cosmicsearch.org>
2017-04-28 17:57:07 -0400

    No blank messages... helps prevent spam a little more.

--- static/chat.js
+++ static/chat.js
@@ -40,15 +40,17 @@ var id = getCookie("username");
 socket.emit("join", [id, cuid]);
 
 function send(msg) {
-  var doc = document.getElementById("window");
+  if (msg != "") {
+    var doc = document.getElementById("window");
 
-  var d = new Date();
-  var hours = ('0' + d.getHours()).slice(-2)
-  var minutes = ('0' + d.getMinutes()).slice(-2)
-  doc.innerHTML += "<b>" + hours + ":" + minutes + "</b> " + msg + "<br>";
-  objDiv = document.getElementById("window");
-  objDiv.scrollTop = objDiv.scrollHeight;
-  notifyMe(msg);
+    var d = new Date();
+    var hours = ('0' + d.getHours()).slice(-2)
+    var minutes = ('0' + d.getMinutes()).slice(-2)
+    doc.innerHTML += "<b>" + hours + ":" + minutes + "</b> " + msg + "<br>";
+    objDiv = document.getElementById("window");
+    objDiv.scrollTop = objDiv.scrollHeight;
+    notifyMe(msg);
+  }
 }
 socket.on("post", function(msg) {
   send(msg);
